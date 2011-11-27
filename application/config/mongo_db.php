31a4e39c461f08726817164edc45919c537ddcb4
railscloud <chakram321@hotmail.com>
2011-11-28 00:55:32 +0530

    Update application/config/mongo_db.php

--- application/config/mongo_db.php
+++ application/config/mongo_db.php
@@ -1,16 +1,11 @@
 <?php
 
-//$config['mongo_host'] = 'localhost';
-//$config['mongo_port'] = 27017;
-//$config['mongo_db'] = 'restserver';
-//$config['mongo_user'] = '';
-//$config['mongo_pass'] = '';
+$config['mongo_host'] = 'localhost';
+$config['mongo_port'] = 27017;
+$config['mongo_db'] = 'restserver';
+$config['mongo_user'] = '';
+$config['mongo_pass'] = '';
 $config['mongo_persist'] = TRUE;
 $config['mongo_persist_key'] = 'ci_mongo_persist';
 
- $config['mongo_host'] = '178.79.145.128';
- $config['mongo_port'] = 37017;
- $config['mongo_db'] = 'restserver';
- $config['mongo_user'] = 'dealtadka';
- $config['mongo_pass'] = 'dealtadka123*';
 ?>
